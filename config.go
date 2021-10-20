f8f2a2d4be398df31d4a375b6aa0e727971457c3
Gökhan Çağrıtekin <gokhan.cagritekin@gmail.com>
2021-10-20 20:21:17 +0300

    gitignore

---
+++ config.go
@@ -0,0 +1,7 @@
+package main
+
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/smtp"
+)
 
 func main() {
-	fmt.Println("first commit")
+
+	from := "my_email@gmail.com"
+	password := "super_secret_password"
+	to := []string{"recipient@email.com"}
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+
+	message := []byte("My super secret message.")
+
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
