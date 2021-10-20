ec6db8d55d9722da9c078faf5d3c045d4d2a23b9
Gökhan Çağrıtekin <gokhan.cagritekin@gmail.com>
2021-10-20 20:23:40 +0300

    delete .env

--- config.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type Config struct {
-	DBDriver      string `mapstructure:"DB_DRIVER"`
-	DBSource      string `mapstructure:"DB_SOURCE"`
-	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
-}
