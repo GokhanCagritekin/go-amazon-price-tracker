6064cbb870aee84cb1bde33bd00b9d65ba8a4356
Gökhan Çağrıtekin <gokhan.cagritekin@gmail.com>
2021-10-20 21:01:45 +0300

    using viper

--- main.go
+++ main.go
@@ -1,24 +1,33 @@
 package main
 
 import (
-	"log"
-	"net/smtp"
+	"fmt"
+	//"net/smtp"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
 
-	from := "my_email@gmail.com"
-	password := "super_secret_password"
-	to := []string{"recipient@email.com"}
-	smtpHost := "smtp.gmail.com"
-	smtpPort := "587"
+	v1 := viper.New()
+	v1.SetConfigFile(".env")
+	v1.ReadInConfig()
+	var variable = v1.GetString("variable")
 
-	message := []byte("My super secret message.")
+	fmt.Println(variable)
 
-	auth := smtp.PlainAuth("", from, password, smtpHost)
+	// from := "my_email@gmail.com"
+	// password := "super_secret_password"
+	// to := []string{"recipient@email.com"}
+	// smtpHost := "smtp.gmail.com"
+	// smtpPort := "587"
 
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// message := []byte("My super secret message.")
+
+	// auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	// err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
