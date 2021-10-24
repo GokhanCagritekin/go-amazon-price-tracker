d12471d06dddf50bfdbe1372cdf7087195e9cb60
Gökhan Çağrıtekin <gokhan.cagritekin@gmail.com>
2021-10-25 01:42:59 +0300

    run periodically - refactor

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/smtp"
 	"os"
 	"strconv"
 	"strings"
@@ -15,30 +16,77 @@ import (
 	"github.com/spf13/viper"
 )
 
+type Mail struct {
+	From     string
+	To       []string
+	Password string
+}
+
 type Tracks struct {
 	DesiredPrice float64 `json:"desiredPrice"`
 }
 
-var ctx = context.Background()
+func LoadMailFields() Mail {
+	vi := viper.New()
+	vi.SetConfigFile(".env")
+	vi.ReadInConfig()
+	From := vi.GetString("from")
+	To := vi.GetStringSlice("to")
+	Password := vi.GetString("password")
+	return Mail{From: From, Password: Password, To: To}
+}
 
-func main() {
+func SendEmail(from string, password string, to []string, message []byte) {
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
 
-	client := redis.NewClient(&redis.Options{
-		Addr:        "localhost:6000",
-		Password:    "",
-		DB:          0,
-		IdleTimeout: 5 * time.Minute,
-	})
+	auth := smtp.PlainAuth("", from, password, smtpHost)
 
-	desiredPrice, _ := strconv.ParseFloat(os.Args[1], 64)
-	url := os.Args[2]
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
-	collector := colly.NewCollector()
+func doEvery(ctx context.Context, d time.Duration, f func(time.Time)) error {
+	ticker := time.Tick(d)
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case x := <-ticker:
+			go f(x)
+		}
+	}
+}
+
+func checkPrices(t time.Time) {
+	client := GetRedisClient()
+
+	keys, _ := client.Keys(ctx, "*http*").Result()
+	for i := 0; i < len(keys); i++ {
+		priceStr := fmt.Sprint(client.Get(ctx, keys[i]))
+		desiredPrice, err := strconv.ParseFloat(priceStr, 64)
+		if err != nil {
+			log.Println(err)
+		}
+		go check(keys[i], desiredPrice, client)
+	}
+}
+
+func check(url string, desiredPrice float64, client *redis.Client) {
+	collector := colly.NewCollector(
+		colly.AllowedDomains("amazon.com", "www.amazon.com"),
+	)
 
 	priceStr := ""
 
 	collector.OnHTML("#priceblock_ourprice", func(element *colly.HTMLElement) {
 		priceStr = element.Text
+	})
+
+	collector.OnError(func(r *colly.Response, err error) {
+		log.Println(err.Error())
 	})
 
 	collector.OnRequest(func(request *colly.Request) {
@@ -51,57 +99,58 @@ func main() {
 		log.Println(err)
 	}
 
-	fmt.Printf("price is %v\n", price)
-
-	v1 := viper.New()
-	v1.SetConfigFile(".env")
-	v1.ReadInConfig()
-
-	from := v1.GetString("from")
-	password := v1.GetString("password")
-	to := v1.GetStringSlice("to")
-
-	message := []byte("My super secret message.")
-
 	if price < desiredPrice {
-		fmt.Printf("email message: %s sent from %s to %s %s", message, from, to, password)
-		// smtpHost := "smtp.gmail.com"
-		// smtpPort := "587"
-
-		// auth := smtp.PlainAuth("", from, password, smtpHost)
-
-		// err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
+		mailFields := LoadMailFields()
+		message := []byte("Price of the Product " + url + "is now lower than " + fmt.Sprint(desiredPrice))
+		SendEmail(mailFields.From, mailFields.Password, mailFields.To, message)
 		client.Del(ctx, url)
-	} else {
-		fmt.Println("too expensive")
+	}
+}
 
-		_, err := client.Get(ctx, url).Result()
-		if err == redis.Nil {
-			fmt.Printf("%s does not exist\n", url)
-			track := Tracks{DesiredPrice: desiredPrice}
-			fmt.Println(track)
-			json, err := json.Marshal(track)
-			if err != nil {
-				log.Println(err)
-			}
+func GetRedisClient() (c *redis.Client) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6000",
+		Password: "",
+		DB:       0,
+	})
+	return client
+}
 
-			err = client.Set(ctx, url, json, 0).Err()
+var ctx = context.Background()
 
-			if err != nil {
-				log.Println(err)
-			}
-		} else if err != nil {
-			panic(err)
-		} else {
-			fmt.Printf("%s does exist\n", url)
+func Addtrack() {
+
+	client := GetRedisClient()
+
+	desiredPrice, _ := strconv.ParseFloat(os.Args[1], 64)
+	url := os.Args[2]
+
+	_, err := client.Get(ctx, url).Result()
+
+	if err == redis.Nil {
+		track := Tracks{DesiredPrice: desiredPrice}
+
+		json, err := json.Marshal(track)
+		if err != nil {
+			log.Println(err)
 		}
-	}
-	keys, _ := client.Keys(ctx, "*http*").Result()
-	for i := 0; i < len(keys); i++ {
-		fmt.Println(keys[i], client.Get(ctx, keys[i]))
-	}
 
+		err = client.Set(ctx, url, json, 0).Err()
+
+		if err != nil {
+			log.Println(err)
+		}
+	} else if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		Addtrack()
+	} else {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		doEvery(ctx, 2000*time.Millisecond, checkPrices)
+	}
 }
